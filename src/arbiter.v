9d874bee44a09090e6bf24efdfe891f83eacac7f
Wei Song <wsong83@gmail.com>
2019-12-09 15:23:22 +0800

    bugfix in the priority arbiters

--- src/arbiter.v
+++ src/arbiter.v
@@ -61,7 +61,7 @@ module ArbPriority
   assign req_o = |req_i;
   generate for(i=0; i<N; i=i+1) begin
     if(i==0) begin
-      assign ack_i[0] = (prio[0] | token[N]) & req_i[0] & ack_o;
+      assign ack_i[0] = (prio[0] | token[N-1]) & req_i[0] & ack_o;
       assign token[0] = prio[0] & ~req_i[0];
       assign token[N] = token[N-1] & ~req_i[0];
     end else begin
@@ -90,7 +90,7 @@ module ArbRR #(parameter N=8)
   assign req_o = |req_i;
   generate for(i=0; i<N; i=i+1) begin
     if(i==0) begin
-      assign ack_i[0] = (prio[0] | token[N]) & req_i[0] & ack_o;
+      assign ack_i[0] = (prio[0] | token[N-1]) & req_i[0] & ack_o;
       assign token[0] = prio[0] & ~req_i[0];
       assign token[N] = token[N-1] & ~req_i[0];
     end else begin
